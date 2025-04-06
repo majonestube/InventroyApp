41184465e0d8904c736dde45fe318753d4daccc5
MajonesTube <martine.vold@hotmail.com>
2025-04-06 14:25:48 +0200

    Ferdig app

--- app/src/main/java/com/example/inventory/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/example/inventory/ui/AppViewModelProvider.kt
@@ -36,7 +36,9 @@ object AppViewModelProvider {
         // Initializer for ItemEditViewModel
         initializer {
             ItemEditViewModel(
-                this.createSavedStateHandle()
+                this.createSavedStateHandle(),
+                inventoryApplication().container.itemsRepository
+
             )
         }
         // Initializer for ItemEntryViewModel
